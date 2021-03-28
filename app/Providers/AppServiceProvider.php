950f5586ee8e5e17b4dfeb8ff2d4d0365d7eb268
Fahad Mahfoth <35464579+fahadmahfoth@users.noreply.github.com>
2021-03-29 02:24:08 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        ini_set("memory_limit", "100M");
+        ini_set('post_max_size', '50M');
+        ini_set('upload_max_filesize', '50M');
     }
 }
